7789f2a2d2072f9ed8e725be494ab6e91bd02793
marktorrano <markstorrano@gmail.com>
2016-07-29 21:04:22 +0800

    Added layout

---
+++ resources/views/head.blade.php
@@ -0,0 +1,7 @@
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}" id="_token" value="{{ csrf_token() }}">
+    <meta id="base_url" data-value="{{url('/')}}">
+    <title>Site Survey Docs</title>
+
+    {{--TODO set if statements for languages--}}
+    <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
