e8fa53c835a109a976229c410a5bfcf154c9ca7d
marktorrano <markstorrano@gmail.com>
2016-08-08 15:36:47 +0800

    Update route file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,35 +6,7 @@ Route::get('/', function () {
 
 Route::group(['middleware' => 'web'], function () {
 
-    Route::post('signatures/{lang}/{docType}', function($lang, $docType, Illuminate\Http\Request $request)
-    {
-
-        $data = json_decode($request->getContent(), true);
-
-//        dd($data);
-
-        if($lang == 'fr' && $docType == 'ssr'){
-
-            return view('documents.ssr-fr', ['data' => $data]);
-
-        }
-        if($lang == 'nl' && $docType == 'ssr'){
-
-            return view('documents.ssr-nl', ['data' => $data]);
-
-        }
-        if($lang == 'fr' && $docType == 'tsa'){
-
-            return view('documents.tsa-fr', ['data' => $data]);
-
-        }
-        if($lang == 'fr' && $docType == 'tsa'){
-
-            return view('documents.tsa-fr', ['data' => $data]);
-
-        }
-
-    });
+    Route::post('signatures/{id}', 'DocumentController@printDocument');
 
 });
 
