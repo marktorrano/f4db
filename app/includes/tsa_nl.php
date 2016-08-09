347417b226f5467c06d899cb6b254c22c62b420a
marktorrano <markstorrano@gmail.com>
2016-08-09 18:01:55 +0800

    Added include files. Translator

---
+++ app/includes/tsa_nl.php
@@ -0,0 +1,50 @@
+<?php
+$t = [
+'doc.title'                                 => 'TSA NL',
+'doc.footerPage'                            => 'Pagina',
+'doc.footerPageOf'                          => 'van',
+'doc.header.dossier'                        => 'Dossier: ',
+'doc.footer.title'                          => 'Proximus NV van publiek recht',
+'doc.footer.adres'                          => 'Koning Albert II-laan 27, B-1030 Brussel, BTW BE 0202.239.951 RPR Brussel, BE50 0001 7100 3118 BPOTBEB1',
+'page01.h2.title'                           => 'Uitrol van het Glasvezelnetwerk',
+'page01.h1.title'                           => 'MDU Technische Oplossing',
+'page01.td.date'                            => 'Datum',
+'page01.td.ourReference'                    => 'Onze referentie',
+'page01.td.contact'                         => 'Contact',
+'page01.td.email'                           => 'E-mail',
+'page02.td.adresses'                        => 'Adres(sen):',
+'page03.h4.generalInformation'              => '1. Algemene informatie',
+'page03.td.visitDate'                       => 'Datum van het bezoek: ',
+'page03.td.fileDate'                        => 'Datum van het bezoek: ',
+'page03.td.responsibleProximus'             => 'Verantwoordelijke Proximus',
+'page03.td.sitename'                        => 'Sitenaam',
+'page03.td.unitsCount'                      => 'Aantal eenheden (totaal)',
+'page03.td.floorsCount'                     => 'Aantal verdiepingen (RDC + x)',
+'page03.h4.yourDetails'                     => '2. Uw gegevens',
+'page03.h6.syndic'                          => 'Syndic',
+'page03.h6.vme'                             => 'VME',
+'page03.h6.owners'                          => 'Eigenaars (of mede-eigenaars)',
+'page03.li.name'                            => 'Naam: ',
+'page03.li.adres'                           => 'Adres: ',
+'page03.li.contact'                         => 'Contact: ',
+'page03.li.email'                           => 'Email: ',
+'page03.li.phone'                           => 'Telefoon: ',
+'page04.h4.informationFirberhoodCadasterIfh'=> '3. Informatie van het Fiberhood / kadaster / IFH <br><small>(voor intern gebruik Proximus)</small>',
+'page04.td.fiberhood'                       => 'Fiberhood',
+'page04.td.cadasterRef'                     => 'Kadaster ref',
+'page04.td.mainLamKey'                      => 'Main LAMKey IFH',
+'page04.h4.networkChanges'                  => '4. Netwerk vernieuwing – Introductie in het gebouw',
+'page04.p.networkChangesText'               => 'In het kader van de vernieuwing van het netwerk zal Proximus het ultrasnelle glasvezelnetwerk tot in de technische ruimte van uw gebouw brengen.',
+'page05.h4.cablingBuilding'                 => '5. Bekabeling van het gebouw',
+'page05.h5.technicalSolutionVertical'       => '5.1 Technische oplossing - Verticale bekabeling',
+'page05.h5.technicalSolutionHorizontal'     => '5.2 Horizontale oplossing - Verticale bekabeling',
+'page05.h6.agreement'                       => '6. Overeenkomst',
+'page05.agreementContent'                   => '<p>Ondergetekende accepteert de installatie van de technische oplossing beschreven in dit document. De algemene richtlijnen van maart 2016 zijn van toepassing.</p>
+<p>Voor akkoord,</p>
+<br>
+<p>Datum                                    :</p>
+<p>Verantwoordelijke van het syndicus / VME  /  Eigenaar</p>
+<p>Naam (in hoofdletters)                   :</p>
+<p>Handtekening                             :</p>',
+];
+?>
