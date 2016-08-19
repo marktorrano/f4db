946a4e8f78a39891f74ac9256edc5af074f728de
marktorrano <markstorrano@gmail.com>
2016-08-19 17:11:48 +0800

    Update: Added proper title for documents

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,4 +1,5 @@
     <meta charset="UTF-8">
-    <title>Site Survey Docs</title>
+    <title>@if(isset($t))<?= $t['doc.title'] ?> @else Site Survey Document @endif</title>
 
     <link rel="stylesheet" href="{{asset('assets/css/app.css')}}"/>
+    <link rel="shortcut icon" href="{{asset('assets/img/favicon.png')}}">
