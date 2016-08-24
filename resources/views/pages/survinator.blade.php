a15c69faebb4096fb8af81cb509600a71e0826a2
marktorrano <markstorrano@gmail.com>
2016-08-24 18:02:57 +0800

    Fix: Including section

--- resources/views/pages/survinator.blade.php
+++ resources/views/pages/survinator.blade.php
@@ -4,40 +4,40 @@
 
 @endpush
 
-<div id="survinator">
-    <div id="form">
-        <h3>Document Generator</h3>
-        <br/>
-
-        <div class="form-group category_field">
-            {!! Form::label('docType', 'Document:') !!}
-            <select id="docType" name="docType" class="form-control">
-                <option value="ssr" selected>Site Survey Report</option>
-            </select>
-        </div>
-
-        {!! Form::open(array('url' => 'documents/', 'target' => '_blank')) !!}
-        <div class="form-group">
-            <div class="form-group">
-                {!! Form::label('id', 'ID:') !!}
-                {!! Form::text('id', '', ['class' => 'form-control']) !!}
-            </div>
-        </div>
-
-        <div class="form-group category_field">
-            {!! Form::label('lang', 'Language:') !!}
-            <select id="lang" name="lang" class="form-control">
-                <option value="nl" selected>Dutch</option>
-                <option value="fr">French</option>
-            </select>
-        </div>
-
-        {!! Form::submit('Generate', ['class' => 'btn btn-primary']) !!}
-        {!! Form::close() !!}
-
-    </div>
-</div>
-
 @section('content')
+
+    <div id="survinator">
+        <div id="form">
+            <h3>Document Generator</h3>
+            <br/>
+
+            <div class="form-group category_field">
+                {!! Form::label('docType', 'Document:') !!}
+                <select id="docType" name="docType" class="form-control">
+                    <option value="ssr" selected>Site Survey Report</option>
+                </select>
+            </div>
+
+            {!! Form::open(array('url' => 'documents/', 'target' => '_blank')) !!}
+            <div class="form-group">
+                <div class="form-group">
+                    {!! Form::label('id', 'ID:') !!}
+                    {!! Form::text('id', '', ['class' => 'form-control']) !!}
+                </div>
+            </div>
+
+            <div class="form-group category_field">
+                {!! Form::label('lang', 'Language:') !!}
+                <select id="lang" name="lang" class="form-control">
+                    <option value="nl" selected>Dutch</option>
+                    <option value="fr">French</option>
+                </select>
+            </div>
+
+            {!! Form::submit('Generate', ['class' => 'btn btn-primary']) !!}
+            {!! Form::close() !!}
+
+        </div>
+    </div>
 
 @endsection
