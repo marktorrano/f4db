55a747a619d785526ff566e21b710474c2e22bb6
marktorrano <markstorrano@gmail.com>
2016-08-24 14:26:18 +0800

    Update: Added csrf token to meta tag

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,4 +1,5 @@
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@if(isset($t))<?= $t['doc.title'] ?> @else Site Survey Document @endif</title>
 
     <link rel="stylesheet" href="{{asset('assets/css/app.css')}}"/>
